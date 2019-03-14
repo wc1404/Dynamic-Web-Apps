a048cf0e5d0e75763f457d76bbbf26106882502a
wc1404 <wc1404@nyu.edu>
2019-03-14 16:29:02 -0400

    Game Bot Commit

---
+++ gameBot/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+	  consumer_key:         '7qp0I6a31s7OivZCdxy4r7k4Z',
+	  consumer_secret:      'dVtju6J7tYSXnNvlzsPTnKVXoD2sodi0lurPcvcakdrBSdZBKd',
+	  access_token:         '1100176342644113409-hT9X2OvJm1yjXVE7bqpZu8qqlF49vX',
+	  access_token_secret:  '0mrDYqbYNgv2UJjUVFqknBOBRpukStJYPVh9dMlwONjq4',
+	  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+	  strictSSL:            true,     // optional - requires SSL certificates to be valid.
+}
